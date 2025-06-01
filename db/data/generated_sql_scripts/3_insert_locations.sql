14e2f4f4f9d72630c269e3fbc05101d273f756b7
Freddy Bautista <abs0rbmejohan3212@gmail.com>
2025-06-01 08:04:00 -0400

    feat(data): add Python scripts and generated SQL files for users, cards, locations, and recharge points data insertion

---
+++ db/data/generated_sql_scripts/3_insert_locations.sql
@@ -0,0 +1,14 @@
+INSERT INTO locations (location_id, name, description) VALUES
+(1, 'Usaquén', 'Zona de operación Usaquén en Bogotá.'),
+(2, 'Chapinero', 'Zona de operación Chapinero en Bogotá.'),
+(3, 'Santa Fe', 'Zona de operación Santa Fe en Bogotá.'),
+(4, 'San Cristóbal', 'Zona de operación San Cristóbal en Bogotá.'),
+(5, 'Usme', 'Zona de operación Usme en Bogotá.'),
+(6, 'Tunjuelito', 'Zona de operación Tunjuelito en Bogotá.'),
+(7, 'Bosa', 'Zona de operación Bosa en Bogotá.'),
+(8, 'Kennedy', 'Zona de operación Kennedy en Bogotá.'),
+(9, 'Fontibón', 'Zona de operación Fontibón en Bogotá.'),
+(10, 'Engativá', 'Zona de operación Engativá en Bogotá.'),
+(11, 'Suba', 'Zona de operación Suba en Bogotá.'),
+(12, 'Barrios Unidos', 'Zona de operación Barrios Unidos en Bogotá.'),
+(13, 'Teusaquillo', 'Zona de operación Teusaquillo en Bogotá.');
